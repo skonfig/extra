ef7a214dae108351e1e2ae66a51966a639ee90d8
Timothée Floure <timothee.floure@posteo.net>
2020-07-21 08:30:09 +0200

    __netbox: support basic LDAP group mapping

--- type/__netbox/files/ldap_config.py.sh
+++ type/__netbox/files/ldap_config.py.sh
@@ -6,7 +6,7 @@ cat << EOF
 ##############################
 
 import ldap
-from django_auth_ldap.config import LDAPSearch
+from django_auth_ldap.config import LDAPSearch, PosixGroupType
 
 # Server URI
 AUTH_LDAP_SERVER_URI = "$LDAP_SERVER"
@@ -15,8 +15,10 @@ AUTH_LDAP_SERVER_URI = "$LDAP_SERVER"
 AUTH_LDAP_BIND_DN = "$LDAP_BIND_DN"
 AUTH_LDAP_BIND_PASSWORD = "$LDAP_BIND_PASSWORD"
 
-# If a user's DN is producible from their username, we don't need to search.
-AUTH_LDAP_USER_DN_TEMPLATE = "$LDAP_USER_DN_TEMPLATE"
+# Search for user entry.
+AUTH_LDAP_USER_SEARCH = LDAPSearch("$LDAP_USER_BASE",
+                                    ldap.SCOPE_SUBTREE,
+                                    "(uid=%(user)s)")
 
 # You can map user attributes to Django attributes as so.
 AUTH_LDAP_USER_ATTR_MAP = {
@@ -25,3 +27,35 @@ AUTH_LDAP_USER_ATTR_MAP = {
     "email": "mail"
 }
 EOF
+
+if [ "$LDAP_GROUP_BASE" != "" ]; then
+	cat << EOF
+
+# This search ought to return all groups to which the user belongs. django_auth_ldap uses this to determine group
+# hierarchy.
+AUTH_LDAP_GROUP_SEARCH = LDAPSearch("$LDAP_GROUP_BASE", ldap.SCOPE_SUBTREE,
+                                    "(objectClass=posixGroup)")
+AUTH_LDAP_GROUP_TYPE = PosixGroupType()
+
+# Mirror LDAP group assignments.
+AUTH_LDAP_MIRROR_GROUPS = True
+EOF
+
+	if [ "$LDAP_REQUIRE_GROUP" != "" ]; then
+		cat << EOF
+
+# Define a group required to login.
+AUTH_LDAP_REQUIRE_GROUP = "$LDAP_REQUIRE_GROUP"
+EOF
+	fi
+
+	if [ "$LDAP_SUPERUSER_GROUP" != "" ]; then
+		cat << EOF
+
+# Define special user types using groups. Exercise great caution when assigning superuser status.
+AUTH_LDAP_USER_FLAGS_BY_GROUP = {
+    "is_superuser": "$LDAP_SUPERUSER_GROUP",
+}
+EOF
+	fi
+fi
