37762dd1cacec8652b142b614d76518bf647a9d8
Timothée Floure <timothee.floure@posteo.net>
2021-02-17 10:52:29 +0100

    [__matrix_synapse_worker] initial implementation

---
+++ type/__matrix_synapse_worker/files/worker.yaml.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+cat << EOF
+worker_app: "${WORKER_APP:?}"
+worker_name: "${WORKER_NAME:?}"
+
+# The replication listener on the main synapse process.
+worker_replication_host: "${WORKER_REPLICATION_HOST:?}"
+worker_replication_http_port: "${WORKER_REPLICATION_PORT:?}"
+
+worker_listeners:
+ - type: http
+   port: "${WORKER_PORT:?}"
+   resources:
+     - names:
+EOF
+
+for resource in ${WORKER_RESOURCES:?}; do
+echo "       - \"$resource\""
+done
+
+cat << EOF
+
+worker_log_config: "${WORKER_LOG_CONFIG:?}"
+EOF
