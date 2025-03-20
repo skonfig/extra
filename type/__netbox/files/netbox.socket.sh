9015f698288f1f69903c550a0f430053e54fd258
Dennis Camera <dennis.camera@riiengineering.ch>
2025-03-20 14:32:35 +0100

    Add missing copyright headers

--- type/__netbox/files/netbox.socket.sh
+++ type/__netbox/files/netbox.socket.sh
@@ -1,10 +1,29 @@
 #!/bin/sh -e
-# __netbox/files/netbox.socket.sh
-
+#
+# 2020 Matthias Stecher (matthiasstecher at gmx.de)
+#
+# This file is part of skonfig-extra.
+#
+# skonfig-extra is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# skonfig-extra is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with skonfig-extra. If not, see <http://www.gnu.org/licenses/>.
+#
+# Generate contents of netbox.socket.
+#
 # This is shared between all WSGI-server types.
-
+#
 # Arguments:
 #  1: File which list all sockets to listen on (sepearated by \n)
+#
 
 if [ $# -ne 1 ]; then
     printf "netbox.socket.sh: argument \$1 missing or too much given!\n" >&2
